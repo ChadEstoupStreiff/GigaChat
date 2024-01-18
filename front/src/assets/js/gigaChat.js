b750ecbbb222980959251cfc44004d06459aedc7
Rexmicool <matthieu.barraud@gmail.com>
2024-01-18 11:07:02 +0100

    Poule

--- front/src/assets/js/gigaChat.js
+++ front/src/assets/js/gigaChat.js
@@ -1,65 +1,84 @@
-let selected_conv = null
-
+let selected_conv = null;
+var ws = null;
+const writting_input = document.getElementById('writting_space_input')
+const writting_button = document.getElementById('writting_space_button')
+const new_chat_receiver_input = document.getElementById('new_chat_receiver_input')
+var new_chat_mode = null;
+const conv_list = document.getElementById("conv_list_space");
+let my_name = "";
+let my_mail = "";
 
 document.getElementById('profile-btn').addEventListener('click', function () {
     checkTokenAndRedirect();
     window.location.href = 'profile.html';
 })
 
-const writting_input = document.getElementById('writting_space_input')
-const writting_button = document.getElementById('writting_space_button')
+
+document.getElementById('add_user_button').addEventListener('click', (event) => {
+    remove_display_none_right_space();
+    remove_display_none_new_receiver_space();
+    new_chat_mode = true;
+    console.log(new_chat_receiver_input.value);
+
+    const chat_list = document.getElementById("chat_list_space");
+    try {
+        const childElements = chat_list.children;
+
+        for (let i = childElements.length - 1; i >= 0; i--) {
+            chat_list.removeChild(childElements[i]);
+        }
+    } catch (error) {
+        console.log("error : ");
+        console.log(error);
+    }
+});
+
+
 
 writting_button.addEventListener('click', (event) => {
     const text = writting_input.value;
     const copiedText = text;
     writting_input.value = '';
-    console.log(copiedText);
 
+    var destinary = "";
+    if (new_chat_mode) {
+        destinary = new_chat_receiver_input.value
+    } else {
+        destinary = selected_conv;
+    }
+    const apiUrlSendMsg = 'http://localhost:4568/chat/user/' + destinary + '?message=' + copiedText;
+    const headersSendMsg = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    };
 
-    
-    
+    // Requête
+    const requestOptionsSendMsg = {
+        method: 'POST',
+        headers: headersSendMsg,
+    };
 
+    // Appel de l'API en utilisant fetch
+    fetch(apiUrlSendMsg, requestOptionsSendMsg)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('ERROR when request to api');
+            }
+            return response.json(); // Renvoie les données de la réponse sous forme de JSON
+        })
+        .catch(error => {
+            // Gestion des erreurs
+            console.error('Erreur:', error);
+        });
 
-                
-                // // API CONNEXION
-                // const apiUrl = 'http://localhost:4568/chats/user/'+selected_conv;
-                // const headers = {
-                //     'Content-Type': 'application/json',
-                //     'Authorization': `Bearer ${localStorage.getItem('token')}`,
-                //     'message': copiedText
-                // };
+    request_conv();
 
-                // // Requête
-                // const requestOptions = {
-                //     method: 'POST',
-                //     headers: headers,
-                // };
-
-                // // Appel de l'API en utilisant fetch
-                // fetch(apiUrl, requestOptions)
-                //     .then(response => {
-                //         if (!response.ok) {
-                //             throw new Error('ERROR when request to api');
-                //         }
-                //         return response.json(); // Renvoie les données de la réponse sous forme de JSON
-                //     })
-                //     .then(data => {
-                //         let user_list = [];
-                //         console.log(data);
-                //     })
-                //     .catch(error => {
-                //         // Gestion des erreurs
-                //         console.error('Erreur:', error);
-                //     });
-
- });
+    onclick_on_conv_name(destinary);
+});
 
 
 
 window.onload = function () {
-    let my_name = "";
-    let my_mail = "";
-
 
     // API CONNEXION
     const apiUrl1 = 'http://localhost:4568/user';
@@ -85,12 +104,33 @@ window.onload = function () {
         .then(data => {
             my_name = data["name"];
             my_mail = data["mail"];
-                    })
+        })
         .catch(error => {
             // Gestion des erreurs
             console.error('Erreur:', error);
         });
 
+
+    request_conv();
+
+
+}
+
+
+
+function request_conv() {
+
+    const conv_list = document.getElementById("conv_list_space");
+    try {
+        const childElements = conv_list.children;
+
+        for (let i = childElements.length - 1; i >= 0; i--) {
+            conv_list.removeChild(childElements[i]);
+        }
+    } catch (error) {
+        console.log("error : ");
+        console.log(error);
+    }
 
     // API CONNEXION
     const apiUrl = 'http://localhost:4568/chats/user';
@@ -115,22 +155,19 @@ window.onload = function () {
         })
         .then(data => {
             let user_list = [];
-            // console.log(data);
+
             data.forEach(chat => {
                 if (my_mail == chat.userA) {
-                    // console.log(chat.userB);
                     user_list.push(chat.userB);
                 }
-                else{
-                    // console.log(chat.userA);
+                else {
                     user_list.push(chat.userA);
                 }
-                
+
             });
 
-            const conv_list = document.getElementById("conv_list_space");
             user_list.forEach(user => {
-                const listItem = document.createElement("button"); // Utilisez des boutons au lieu de list items
+                const listItem = document.createElement("button");
                 listItem.textContent = user;
                 listItem.setAttribute("user_mail", user);
                 conv_list.appendChild(listItem);
@@ -138,82 +175,8 @@ window.onload = function () {
 
             // Gestionnaire d'événements pour les éléments de la liste du calendrier
             conv_list.addEventListener("click", function (event) {
-
-
-                // // Create WebSocket connection.
-                // const socket = new WebSocket('http://localhost:4568/chats/user/'+selected_conv);
-
-                // // // Connection opened
-                // // socket.addEventListener("open", (event) => {
-                // // socket.send("Hello Server!");
-                // // });
-
-                // // Listen for messages
-                // socket.addEventListener("message", (event) => {
-                //     console.log("Message from server ", event.data);
-                // });
-
-                var ws = new WebSocket('ws://127.0.0.1:4568/ws/chat/user/'+selected_conv + '?token=' + localStorage.getItem('token'));
-                ws.onmessage = function(event) {
-                    console.log("Message from server ", event.data);
-                };
-
-                
                 selected_conv = event.target.getAttribute("user_mail");
-                // console.log(selected_conv);
-                
-                // API CONNEXION
-                const apiUrl2 = 'http://localhost:4568/chat/user/'+selected_conv;
-                const headers2 = {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
-                };
-
-                // Requête
-                const requestOptions2 = {
-                    method: 'GET',
-                    headers: headers2,
-                };
-
-                // Appel de l'API en utilisant fetch
-                fetch(apiUrl2, requestOptions2)
-                    .then(response => {
-                        if (!response.ok) {
-                            throw new Error('ERROR when request to api');
-                        }
-                        return response.json(); // Renvoie les données de la réponse sous forme de JSON
-                    })
-                    .then(data => {
-                        // console.log(data);
-                        messages = data["messages"];
-                        
-                        const chat_list = document.getElementById("chat_list_space");
-                        console.log(chat_list);
-                        try {
-                            const childElements = chat_list.children;
-                                                
-                            for (let i = childElements.length - 1; i >= 0; i--) {
-                                chat_list.removeChild(childElements[i]);
-                            }
-                        } catch (error) {
-                            console.log("error : ");
-                            console.log(error);
-                        }
-
-                        messages.forEach(msg => {
-                            const listItem = document.createElement("p"); // Utilisez des boutons au lieu de list items
-                            listItem.textContent = msg.message;
-                            listItem.setAttribute("name", msg.name);
-                            chat_list.appendChild(listItem);
-                        })
-
-                                })
-                    .catch(error => {
-                        // Gestion des erreurs
-                        console.log("error : ");
-                        console.log(error);
-                    });
-
+                onclick_on_conv_name(selected_conv);
 
             });
         })
@@ -222,3 +185,94 @@ window.onload = function () {
             console.error('Erreur:', error);
         });
 }
+
+
+
+function onclick_on_conv_name(selected_conv) {
+    remove_display_none_right_space();
+    set_display_none_new_receiver_space();
+    new_chat_mode = false;
+
+    if (ws != null) {
+        ws.close();
+    }
+    ws = new WebSocket('ws://127.0.0.1:4568/ws/chat/user/' + selected_conv + '?token=' + localStorage.getItem('token'));
+    ws.onmessage = function (event) {
+        data = JSON.parse(event.data)
+        let lItem = document.createElement("p");
+        lItem.textContent = data.msg;
+        lItem.setAttribute("name", data.name);
+        let c_list = document.getElementById("chat_list_space");
+        c_list.appendChild(lItem);
+    };
+
+
+    // API CONNEXION
+    const apiUrl2 = 'http://localhost:4568/chat/user/' + selected_conv;
+    const headers2 = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    };
+
+    // Requête
+    const requestOptions2 = {
+        method: 'GET',
+        headers: headers2,
+    };
+
+    // Appel de l'API en utilisant fetch
+    fetch(apiUrl2, requestOptions2)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('ERROR when request to api');
+            }
+            return response.json(); // Renvoie les données de la réponse sous forme de JSON
+        })
+        .then(data => {
+            // console.log(data);
+            messages = data["messages"];
+
+            const chat_list = document.getElementById("chat_list_space");
+            console.log(chat_list);
+            try {
+                const childElements = chat_list.children;
+
+                for (let i = childElements.length - 1; i >= 0; i--) {
+                    chat_list.removeChild(childElements[i]);
+                }
+            } catch (error) {
+                console.log("error : ");
+                console.log(error);
+            }
+
+            messages.forEach(msg => {
+                const listItem = document.createElement("p"); // Utilisez des boutons au lieu de list items
+                listItem.textContent = msg.message;
+                listItem.setAttribute("name", msg.name);
+                chat_list.appendChild(listItem);
+            })
+
+        })
+        .catch(error => {
+            // Gestion des erreurs
+            console.log("error : ");
+            console.log(error);
+        });
+}
+
+
+
+function remove_display_none_right_space() {
+    document.getElementById("right_space").style.display = "block";
+}
+
+
+function remove_display_none_new_receiver_space() {
+    document.getElementById("new_chat_receiver_space").style.display = "block";
+}
+
+
+function set_display_none_new_receiver_space() {
+    document.getElementById("new_chat_receiver_space").style.display = "none";
+}
+
